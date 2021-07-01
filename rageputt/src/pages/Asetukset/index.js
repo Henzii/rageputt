aa06835fdad750d96a9a66b2cbe79803daeccf32
Henzii <80477931+Henzii@users.noreply.github.com>
2021-07-01 17:43:56 +0300

    Build

--- rageputt/src/pages/Asetukset/index.js
+++ rageputt/src/pages/Asetukset/index.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/client"
-import { Backdrop, CircularProgress, Grid, TextField, } from "@material-ui/core"
+import { Backdrop, CircularProgress, Grid, TextField, Container, Typography, Paper } from "@material-ui/core"
 import { GET_ME } from "../../queries"
 
 const Asetukset = () => {
@@ -13,20 +13,22 @@ const Asetukset = () => {
         )
     }
     return (
-        <div>
-            <h1>Tiedot</h1>
-            <Grid container>
-                <Grid item xs={6}>Tunnus</Grid>
-                <Grid item xs={6}>{user.data.getMe.user}</Grid>
-                <Grid item xs={6}>Nimi</Grid>
-                <Grid item xs={6}>{user.data.getMe.name}</Grid>
-                <Grid item xs={6}>Sähköposti</Grid>
-                <Grid item xs={6}>{user.data.getMe.email}</Grid>
-            </Grid>
-            <h2>Vaihda salasana</h2>
-            <TextField type="password" variant="outlined" label="Uusi salasana" fullWidth size="small"/>
-            <TextField type="password" variant="outlined" label="Salasana uudestaan" fullWidth size="small"/>
-        </div>
+        <Container>
+            <Typography variant="h4" gutterBottom>Tiedot</Typography>
+            <Typography>
+                <Grid container component={Paper} className="paperi">
+                    <Grid item xs={6}>Tunnus</Grid>
+                    <Grid item xs={6}>{user.data.getMe.user}</Grid>
+                    <Grid item xs={6}>Nimi</Grid>
+                    <Grid item xs={6}>{user.data.getMe.name}</Grid>
+                    <Grid item xs={6}>Sähköposti</Grid>
+                    <Grid item xs={6}>{user.data.getMe.email}</Grid>
+                </Grid>
+                <Typography variant="h5" gutterBottom>Vaihda salasana</Typography>
+                <TextField type="password" variant="outlined" label="Uusi salasana" fullWidth size="small" />
+                <TextField type="password" variant="outlined" label="Salasana uudestaan" fullWidth size="small" />
+            </Typography>
+        </Container>
     )
 
 }
