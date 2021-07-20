59e33c9fa8f39168f603972a4f7b7506ba92ef7f
Henzii <80477931+Henzii@users.noreply.github.com>
2021-07-21 01:02:54 +0300

    tyylejä lisää

--- rageputt/src/pages/Peli/Tulosruutu.js
+++ rageputt/src/pages/Peli/Tulosruutu.js
@@ -1,4 +1,4 @@
-import { Grid, IconButton } from '@material-ui/core'
+import { Grid, IconButton, Typography } from '@material-ui/core'
 import { ChevronLeft, ChevronRight } from '@material-ui/icons'
 import { useDispatch } from 'react-redux'
 import Player from './Player'
@@ -47,8 +47,8 @@ const Tulosruutu = ({ kierrosData, tulokset }) => {
     return (
         <div>
             <Grid container className="rundiValitsin">
-                <Grid item ><IconButton onClick={() => dispatch({ type: 'DEC_ROUND' })}><ChevronLeft /></IconButton></Grid>
-                <Grid item component={'h2'}>Round {kierros + 1}</Grid>
+                <Grid item><IconButton size="large" onClick={() => dispatch({ type: 'DEC_ROUND' })}><ChevronLeft /></IconButton></Grid>
+                <Grid item component={'h2'}> {kierros + 1}/20</Grid>
                 <Grid item><IconButton onClick={() => dispatch({ type: 'INC_ROUND' })}><ChevronRight /></IconButton></Grid>
             </Grid>
             {kierrosData.data.getRound.players.map(p => <Player key={p.user.user + 'ppp'} player={p} round={kierros} />)}
